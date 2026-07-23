a728ebb77e973bdb5108f54a60e158956e984698
Neel <neelarmstrong33@gmail.com>
2026-07-23 08:00:36 +0530

    Creating MySecurityFilterChain rather than builtinSecurityChain

---
+++ SecurityApplication/src/main/java/com/codeburner/SecurityApplication/services/UserService.java
@@ -0,0 +1,23 @@
+package com.codeburner.SecurityApplication.services;
+
+import com.codeburner.SecurityApplication.exceptions.ResourceNotFoundException;
+import com.codeburner.SecurityApplication.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+//@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User with email " + username + " not found"));
+    }
+
+}
